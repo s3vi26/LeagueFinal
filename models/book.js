fded534bb58899d8751bc5986f8aab4939e9f8f1
Todd Pickell <tapickell@gmail.com>
2018-07-16 10:26:45 -0700

    got caching to work

--- models/book.js
+++ models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const sumSchema = new Schema({
   name: { type: String, required: true },
   accountId: {type: Number, required: true},
-  ttl: { type: Date, default: Date.now() + 100000 }
+  ttl: { type: Date, default: Date.now() + 1000000 }
 });
 
 const summonerz = mongoose.model("summonerz", sumSchema);
--- models/summoner.js
+++ models/summoner.js
@@ -17,26 +17,22 @@ let client = {
   },
   // {accountId: ...}
   accountBySummonerName: function(summoner_name) {
-      let summonerId
-      Summoner.
-        find({name: summoner_name, ttl: { $gt: Date.now() }}).
-        limit(1).
-        select('accountId').
-        then((accountId) => { summonerID = accountId })
-    if (summonerId) {
-        console.log(summonerId, "Got summoner from DB")
-        return Promise.new(summonerId)
-    } else {
-        let url = `${base_api_url}${summ_api_url}/${summoner_name}${API_KEY}`
-        console.log("API REQ: ", url)
-        axios.get(url).
-                then((body) => {
-                    Summoner.create({name: summoner_name, accountId: body.data.accountId}, (error, summoner) => {
-                        if (error) {console.log(error, "SHIT BROKE TRYING TO CREATE MODEL")}
-                        console.log(summoner, "Stored summoner in DB")
-                        return Promise.new(body.data.accountId)
-                })
-    }
+      return Summoner.find({name: summoner_name, ttl: { $gt: Date.now() }}).limit(1).select('accountId').then((summFound) => {
+            if (summFound.length > 0) {
+                console.log(summFound, 'Found summoner in db')
+                return Promise.resolve(summFound[0].accountId)
+            } else {
+                let url = `${base_api_url}${summ_api_url}/${summoner_name}${API_KEY}`
+                console.log("API REQ: ", url)
+                return axios.get(url).then((body) => {
+                            Summoner.create({name: summoner_name, accountId: body.data.accountId}, (error, summoner) => {
+                                if (error) {console.log(error, "SHIT BROKE TRYING TO CREATE MODEL")}
+                                console.log(summoner, "Stored summoner in DB")
+                                return Promise.resolve(body.data.accountId)
+                        })
+                    })
+            }
+        })
   }
 }
 
