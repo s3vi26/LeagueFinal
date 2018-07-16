bb8f83b023f7fef2df4ec6381208dddffc018b09
Todd Pickell <tapickell@gmail.com>
2018-07-15 23:01:00 -0700

    lets point to our actual server

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 export default {
   getMatches: function(params) {
-    return axios.post("/api/summoners/matches/", params);
+    return axios.post("http://api.lolmatcher.com/api/summoners/matches/", params);
   }
 }
